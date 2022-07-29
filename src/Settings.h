3719fa1b38afe8709e828da7a1e6eab5c709bbde
Emil Gajšak <emil.gajsak@gmail.com>
2022-07-29 16:35:42 +0200

    Removed unused constants in Settings

--- src/Settings.h
+++ src/Settings.h
@@ -5,9 +5,6 @@
 
 #define SHUTDOWN_STEPS 5
 
-extern const uint32_t ShutdownSeconds[SHUTDOWN_STEPS];
-extern const char* ShutdownText[SHUTDOWN_STEPS];
-
 struct SettingsData {
 	uint32_t shutdownTime = 4;
 	uint8_t screenBrightness = 200;

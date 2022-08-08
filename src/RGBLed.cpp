35ab1071c3f9ea88f64c8cb422936f6baadf40dc
Emil Gajšak <emil.gajsak@gmail.com>
2022-08-08 13:32:05 +0200

    Fixed RGB led wrong channel

--- src/RGBLed.cpp
+++ src/RGBLed.cpp
@@ -5,6 +5,10 @@
 RGBLed RGB;
 
 void RGBLed::begin(){
+	pinMode(RGB_R, OUTPUT);
+	pinMode(RGB_G, OUTPUT);
+	pinMode(RGB_B, OUTPUT);
+
 	ledcSetup(0, 5000, 8);
 	ledcSetup(1, 5000, 8);
 	ledcSetup(2, 5000, 8);
@@ -20,7 +24,7 @@ const Pixel& RGBLed::getColor() const{
 
 void RGBLed::setColor(const Pixel& color){
 	RGBLed::color = color;
-	ledcWrite(RGB_R, color.r);
-	ledcWrite(RGB_G, color.g);
-	ledcWrite(RGB_B, color.b);
+	ledcWrite(0, 255 - color.r);
+	ledcWrite(1, 255 - color.g);
+	ledcWrite(2, 255 - color.b);
 }
